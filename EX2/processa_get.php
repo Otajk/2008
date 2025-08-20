4c46869aad61211514a38028ca9a190a293d7232
= <=>
2025-08-20 17:54:59 -0300

    COMIT 2 20/08

---
+++ EX2/processa_get.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <style>
+        form{
+            width: max-content;
+        }
+    </style>
+<div id="container">
+<form action="processa_get.php" method="get">
+<?php
+    if(isset($_GET['nome']) && !empty($_GET['nome'])){
+        $nome = htmlspecialchars($_GET['nome']);
+        echo "<h1>Bem vindo, $nome!</h1>";
+
+    }else{
+        header("Location: index.html");
+        exit();
+    }
+
+    if(isset($_GET['nome']) && !empty($_GET['nome'])){
+        $nome = htmlspecialchars($_GET['email']);
+        echo "<h1>Email: $nome</h1>";
+
+    }else{
+        header("Location: index.html");
+        exit();
+    }
+
+    if(isset($_GET['nome']) && !empty($_GET['nome'])){
+        $nome = htmlspecialchars($_GET['senha']);
+        echo "<h1>Senha: $nome</h1>";
+
+    }else{
+        header("Location: index.html");
+        exit();
+    }
+
+    if(isset($_GET['nome']) && !empty($_GET['nome'])){
+        $nome = htmlspecialchars($_GET['telefone']);
+        echo "<h1>Telefone: $nome</h1>";
+
+    }else{
+        header("Location: index.html");
+        exit();
+    }
+    if(isset($_GET['botao'])){
+        header("Location: index.html");
+        exit();
+    }
+
+?>
+
+<button type="submit" name="botao">Sair</button>
+</form>
+</div>
+</body>
+</html>
